27aed76eb1041435ba601a3d75c2078e29ea1cbd
Jimmaa <irs.info100@gmail.com>
2025-09-29 06:38:26 -0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-    base: "./",
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",   // makes assets load correctly on Vercel
+  build: {
+    rollupOptions: {
+      input: 'index.html',   // entry file
+    }
+  }
+})
